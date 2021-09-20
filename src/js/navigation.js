90d4c7a11f3e59be177e6d49f45fc4f3c2c51719
James Minor <james.minor.business@gmail.com>
2021-09-20 11:22:11 -0500

    Added information footer.

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,36 +1,35 @@
 /* Controls the Sidebar navigation menus.
 */
 const sections = document.getElementsByTagName("section");
-var scrollBreakPoints = [];
+var scrollBreakPoints = [0];
 
-for(var i = 0; i < sections.length; i++)
+for(var i = 1; i < sections.length; i++)
 {
 	scrollBreakPoints.push(sections[i].offsetTop + 15);
 }
 
-window.addEventListener('resize', function(event)
+window.addEventListener("resize", function(event)
 {
-	scrollBreakPoints = [];
+	scrollBreakPoints = [0];
 
-	for(var i = 0; i < sections.length; i++)
+	for(var i = 1; i < sections.length; i++)
 	{
 		scrollBreakPoints.push(sections[i].offsetTop);
 	}
 }, true);
 
-const navigationList = document.getElementById("internal-links").getElementsByTagName("a");
-var menuOpened = false;
+const navigationList = document.getElementById("scroll-selectors").getElementsByTagName("a");
 
 // --- OnClick Event Callbacks ---
 
 navigationList[0].onclick = function navigate()
 {
-	window.scrollTo(0, scrollBreakPoints[2] + 87);
+	window.scrollTo(0, scrollBreakPoints[2] + 85);
 }
 
 navigationList[1].onclick = function navigate()
 {
-	window.scrollTo(0, scrollBreakPoints[1] + 80);
+	window.scrollTo(0, scrollBreakPoints[1] + 85);
 }
 
 navigationList[2].onclick = function navigate()
@@ -38,6 +37,8 @@ navigationList[2].onclick = function navigate()
 	window.scrollTo(0, scrollBreakPoints[0]);
 }
 
+// --- Opening the mobile navigation menu ---
+var menuOpened = false;
 function openMobileNavigationMenu(button)
 {
 	button.classList.toggle("menu-opened");
